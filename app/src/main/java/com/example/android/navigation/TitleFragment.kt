dabebea3a870b7ea2ceee23f4670aecfef38c07d
Yuki Sumida <y.sumida.0201@gmail.com>
2019-03-11 21:43:51 +0900

    Aboutメニューを追加

--- app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -2,13 +2,12 @@ package com.example.android.navigation
 
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
 /**
@@ -23,9 +22,18 @@ class TitleFragment : Fragment() {
         binding.playButton.setOnClickListener { view: View ->
             Navigation.findNavController(view).navigate(R.id.action_titleFragment_to_gameFragment)
         }
-
+        setHasOptionsMenu(true)
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater?.inflate(R.menu.overflow_menu, menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return NavigationUI.onNavDestinationSelected(item!!,
+                Navigation.findNavController(view!!))
+                || super.onOptionsItemSelected(item)
+    }
 }
